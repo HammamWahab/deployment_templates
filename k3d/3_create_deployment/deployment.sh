d857070b0c054240126be99fc4e1e01d3b8205e5
Hammam Abdelwahab <hammam9497@gmail.com>
2024-10-20 13:06:08 +0200

    Added Persisten Volume/Claim. Added DB service. Added Kustomization.

--- k3d/3_create_deployment/deployment.sh
+++ k3d/3_create_deployment/deployment.sh
@@ -1,9 +1,9 @@
 #!/usr/bin
 
-export DEPLOYMENT=app-deploy 
+export DEPLOYMENT=deploy-app
 export REGISTRY_NAME=k3d-test-app-registry
-export REGISTRY_PORT= 5050
-export IMAGE=k3d-test-app-registry:5050/deploy:v0.1
+export REGISTRY_PORT=5050
+export IMAGE=${REGISTRY_NAME}:${REGISTRY_PORT}/postgres:v0.1
 
 
 kubectl create deployment ${DEPLOYMENT} --image=${IMAGE}
